8c2f7309bdc79c7430cde659943e74f875659c2d
Jeff <tabirijeffrey@gmail.com>
2025-12-15 20:15:29 +0100

    chore: update example.cpp

--- example/example.cpp
+++ example/example.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <Vector2.h>
 #include <Vector3.h>
 
@@ -5,6 +6,10 @@ int main(int argc, char *argv[]) {
     jade::Vector3 A = jade::Vector3(4,5,6);
 
     jade::Vector3 B = jade::Vector3(10, 20, 10);
+
+    jade::Vector3 C = A + B;
+
+    std::cout << C.x << " " << C.y << " " << C.z << std::endl;
 
     return 0;
 }
