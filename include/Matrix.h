93c883e2ce0f3d971ee45a6140ad6bcf22655c11
Jeff <tabirijeffrey@gmail.com>
2026-01-04 16:41:58 +0100

    feat: create helper methods for Matrix.h

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,10 +1,5 @@
-//
-// Created by Solarium on 29/12/2025.
-//
-
 #ifndef JADE_MATRIX_H
 #define JADE_MATRIX_H
-#include <stdexcept>
 
 template<typename T, int Rows, int Cols>
 class Matrix {
@@ -14,6 +9,7 @@ class Matrix {
 
 public:
     Matrix() = default;
+
     Matrix(std::initializer_list<std::initializer_list<T>> init) {
         int r = 0;
         for (const auto& row : init) {
@@ -34,7 +30,7 @@ public:
             }
         }
         return result;
-    };
+    }
 
     Matrix<T, Rows, Cols> operator-(const Matrix<T, Rows, Cols>& other) const {
         Matrix<T, Rows, Cols> result;
@@ -46,38 +42,68 @@ public:
         return result;
     };
 
-    Matrix<T, Rows, Cols> operator*(T scalar) const {
+    friend bool operator==(const Matrix& mat1, const Matrix& mat2) {
+        bool equal = true;
+        for (int i = 0; i < Rows; ++i) {
+            for (int j = 0; j < Cols; ++j) {
+                equal = equal && mat1.data[i][j] == mat2.data[i][j];
+            }
+        }
+        return equal;
+    }
+
+    friend bool operator!=(const Matrix& mat1, const Matrix& mat2)
+    {
+        return !(mat1 == mat2);
+    }
+
+
+    Matrix<T, Rows, Cols> operator-() const {
         Matrix<T, Rows, Cols> result;
         for (int i = 0; i < Rows; ++i) {
             for (int j = 0; j < Cols; ++j) {
-                result.data[i][j] = data[i][j] * scalar;
+                result.data[i][j] = -data[i][j];
             }
         }
         return result;
     }
 
-    Matrix<T, Rows, Cols> operator/(T scalar) const {
+    friend Matrix operator*(T scalar, const Matrix& mat1) {
         Matrix<T, Rows, Cols> result;
         for (int i = 0; i < Rows; ++i) {
             for (int j = 0; j < Cols; ++j) {
-                result.data[i][j] = data[i][j] / scalar;
+                result.data[i][j] = mat1.data[i][j] * scalar;
             }
         }
         return result;
     }
 
-    Matrix operator*(const Matrix& other) const {
-        if (cols != other.rows) {
-            throw std::runtime_error("Matrix dimensions are not fit for matrix multiplication");
+    friend Matrix operator*(const Matrix& mat1, T scalar) {
+        return scalar * mat1;
+    }
+
+    friend Matrix operator/(T scalar, const Matrix& mat1) {
+        Matrix<T, Rows, Cols> result;
+        for (int i = 0; i < Rows; ++i) {
+            for (int j = 0; j < Cols; ++j) {
+                result.data[i][j] = mat1.data[i][j] / scalar;
+            }
         }
+        return result;
+    }
 
-        Matrix<T, other.rows, this->cols> result;
+    friend Matrix operator/(const Matrix& mat1, T scalar) {
+        return scalar * mat1;
+    }
 
-        for (int i = 0; i < this->rows; i++) {
-            for (int j = 0; j < other.cols; j++) {
-                result[i][j] = 0;
-                for (int k = 0; k < other.rows; k++) {
-                    result[i][j] += data[i][k] * other.data[k][j];
+    template<int OtherCols>
+    Matrix<T, Rows, OtherCols> operator*(const Matrix<T, Cols, OtherCols>& other) const {
+        Matrix<T, Rows, OtherCols> result{};
+
+        for (int i = 0; i < Rows; i++) {
+            for (int j = 0; j < OtherCols; j++) {
+                for (int k = 0; k < Cols; k++) {
+                    result.data[i][j] += data[i][k] * other.data[k][j];
                 }
             }
         }
@@ -85,7 +111,7 @@ public:
         return result;
     }
 
-    friend std::ostream& operator<< (std::ostream& out, const Matrix& mat) {
+    friend std::ostream& operator<<(std::ostream& out, const Matrix& mat) {
         for (int i = 0; i < mat.rows; i++) {
             for (int j = 0; j < mat.cols; j++) {
                 if (j == mat.cols - 1) { // if the number is last in its entry
@@ -96,10 +122,21 @@ public:
             }
             out << "\n";
         }
-
         return out;
     }
 
+    friend std::ostream& operator>> (std::ostream& in, const Matrix& mat) {
+        for (int i = 0; i < mat.rows; i++) {
+            for (int j = 0; j < mat.cols; j++) {
+                in >> mat.data[i][j];
+            }
+        }
+        return in;
+    }
+
+    T* operator[](int idx) {
+        return data[idx];
+    }
 };
 
 
